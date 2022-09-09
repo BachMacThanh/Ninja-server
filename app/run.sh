be4778fbeed25daee3e1a350f3710db6cbe085ff
DangDev <dream858vip@gmail.com>
2022-09-09 10:25:38 +0000

    update

--- app/run.sh
+++ app/run.sh
@@ -1,4 +1,4 @@
 sudo service apache2 start
 sudo service mariadb start
 # java -server -jar -Dfile.encoding=UTF-8 -Xms700M -Xmx700M dist/NSO-HOIUC.jar
-java -server -jar -Dfile.encoding=UTF-8 -Xms2G -Xmx2G app.jar
+java -server -jar -Dfile.encoding=UTF-8 -Xms1G -Xmx1G app.jar
